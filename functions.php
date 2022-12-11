322244b48aa93ff057b036613e856271eb0a133c
jtzuya <jtzuya@gmail.com>
2022-12-11 12:59:14 +0800

    functions & scoping

--- functions.php
+++ functions.php
@@ -2,9 +2,37 @@
     // TO CHECK THIS PAGE VISIT
     // http://localhost:PORT/functions.php
 
-    // TODOOOOOOOOOOOS
+    $str = 'functions ';
 
-    // CONTINUE TOMORROW
-    // https://www.youtube.com/watch?v=438PsnpJj5E&list=PL4cUxeGkcC9gksOX3Kd9KPo-O68ncT05o&index=13
-    // check php guide to see how to spin up the PHP server again
+    // echo strlen($str);
+    // strlen and count is very different
+
+    // $str = 'Broo', here is that we are passing a default value of our $str param so that whenever if we do not have an argument that's being passed to the function and at the 
+    // same time using the parameter inside the body of our function, we can still use it without having any error
+    function firstLetterToUppercase($str = 'Broo') {
+        echo "Hey $str";
+    }
+    
+    firstLetterToUppercase($str); // and if we are passing an argument here, it'll use that value rather than the default value
+
+
+    /*
+        caveat: 
+        In PHP, while using i.e $product['name'] inside a string like this "$product['name']" is that it won't return anything and creates an error.
+        We need to wrap them inside a curly braces in order for it to be working properly like this "{$product['name']}"
+        It's like the template string `` in javascript
+        I.e
+    */
+
+    function formatProduct($product) {
+        // echo "<strong>{$product['name']}</strong> costs {$product['cost']} USD to buy <br />";
+        return "<strong>{$product['name']}</strong> costs {$product['cost']} USD to buy <br />";
+    }
+    $output = formatProduct(['name' => 'Macbook Pro', 'cost' => '1200.00']);
+    echo '<br />' . $output;
+    
+
+
+
+    // echo 'test';
 ?>
